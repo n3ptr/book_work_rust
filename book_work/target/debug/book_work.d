8eb9a91312b7f6dfc0f8bf0da7ed0cf9c227b7e2
nic <nic@pop-os.localdomain>
2020-08-08 10:50:04 -0500

    rust book tutorials, made new cargo run

---
+++ book_work/target/debug/book_work.d
@@ -0,0 +1,1 @@
+/home/nic/github/book_work_rust/book_work/target/debug/book_work: /home/nic/github/book_work_rust/book_work/src/main.rs
---
+++ book_work/target/debug/deps/book_work-b01bb5759c9e4104.d
@@ -0,0 +1,5 @@
+/home/nic/github/book_work_rust/book_work/target/debug/deps/book_work-b01bb5759c9e4104: src/main.rs
+
+/home/nic/github/book_work_rust/book_work/target/debug/deps/book_work-b01bb5759c9e4104.d: src/main.rs
+
+src/main.rs:
